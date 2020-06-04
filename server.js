7da8118c52411801709aa159845e9252db926c69
roberto.torres <ts.roberto@gmail.com>
2020-06-04 01:41:57 +0100

    cypress e2e fixed

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const path = require('path')
 const app = express()
 app.use(express.static(path.join(__dirname, 'build')))
 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 app.get('/v1/method', function (req, res) {
   const var1 = req.query.var1
   let file = ''
